febc356ab2e7571f4a5600ec6b2e75c4d047ed78
Kashif ali <pandagame112211@gmail.com.com>
2026-08-21 10:08:37 +0500

    goo

--- notes/src/App.jsx
+++ notes/src/App.jsx
@@ -10,6 +10,16 @@ export default function App() {
        console.log(data.results);
        
       }}>Get Photos</button>
+       <button className='bg-white rounded p-3 m-5 text-black' onClick={async()=>{
+       const data = await fetchPhotos('cat')
+       console.log(data.results);
+       
+      }}>Get Photos</button>
+       <button className='bg-white rounded p-3 m-5 text-black' onClick={async()=>{
+       const data = await fetchPhotos('cat')
+       console.log(data.results);
+       
+      }}>Get Photos</button>
     </div>
   )
 }
